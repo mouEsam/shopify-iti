20462bf489bfd5735e17d5a864da6c4871d8fc30
Moustafa Essam <drsh_201122@yahoo.com>
2023-06-08 20:17:25 +0300

    add base graphql endpoint

--- Shopify-ITI/Core/Services/Environment/Environment.swift
+++ Shopify-ITI/Core/Services/Environment/Environment.swift
@@ -8,5 +8,13 @@
 import Foundation
 
 struct EnvironmentProvider: AnyEnvironmentProvider {
-    let shopifyBaseUrl: String = "https://localhost"
+    
+    var shopifyStorefrontAccessToken: String
+    
+    var shopifyStorefrontAPIKey: String
+    
+    var shopifyBaseUrl: String {
+        "https://\(shopifyStorefrontAPIKey):\(shopifyStorefrontAccessToken)@mad43-alex-ios-team1.myshopify.com/api/2022-01/graphql.json"
+    }
+    
 }
